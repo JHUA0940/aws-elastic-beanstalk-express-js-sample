14b2186f986f3ea783b6f7818e4c0493cf6297fe
jhua0940 <jhua0940@gmail.com>
2024-09-24 14:44:01 +0800

    update app.js

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
-// 修改监听地址为 '0.0.0.0'，确保容器内可以被外部访问
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`App running on http://0.0.0.0:${port}`);
+    console.log(`App running on http://0.0.0.0:${port}`);
 });
